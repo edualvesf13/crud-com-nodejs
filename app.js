9fc79d8d89e0a42e21992304313807cc1d0da2e3
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-11-19 19:22:37 -0300

    Created Routes and Template.

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
-const handleBars = require('handlebars')
+const handlebars = require('express-handlebars')
 const app = express()
+
+
+// Template Engine
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+// Routes and Templates
+app.get('/:id?', function (req, res) {
+    //res.send('Essa é minha página inicial!')
+    //res.sendFile(__dirname+"/index.html")
+    res.render('index', {id:req.params.id})
+})
 
 
 // Start Server
